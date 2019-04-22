7c266031b9ea008186789bb63d2c9297b1e49f64
isamarskiy <samarskyii@gmail.com>
2019-04-22 18:46:11 +0300

    browsers_fixture_first

---
+++ HW4_api/test_api_dog.py
@@ -0,0 +1,36 @@
+""" API tests for https://dog.ceo/dog-api/"""
+
+import pytest
+
+ENDPOINTS = ['breeds/list/all',
+             'breed/akita/images/random',
+             'breeds/image/random',
+             'breeds/image/random/51',
+             'breed/hound/list',
+             'breed/hound/images',
+             'breed/hound/images/random',
+             'breed/hound/images/random/3',
+             'breed/hound/afghan/images',
+             'breed/hound/afghan/images/random',
+             'breed/hound/afghan/images/random/3']
+
+
+@pytest.mark.parametrize('endpoint', ENDPOINTS)
+def test_all_endpoints(dog, endpoint):
+    response = dog.get(endpoint)
+    data = response.json()
+    assert response.status_code == 200
+    assert data['status'] == 'success'
+    assert data['message'] is not None
+
+
+@pytest.mark.parametrize('endpoint', ENDPOINTS[3:4])
+def test_random_im(dog, endpoint):
+    """
+    Test max value for DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION (max 50)
+    """
+    response = dog.get(endpoint)
+    data = response.json()
+    assert response.status_code == 200
+    assert data['status'] == 'success'
+    assert len(data['message']) == 50
