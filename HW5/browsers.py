01902f58b1104e9b4118062c0273683f32cf2ba6
isamarskiy <samarskyii@gmail.com>
2019-04-29 16:08:07 +0300

    first test

--- HW5/browsers.py
+++ HW5/browsers.py
@@ -1,6 +1,10 @@
 def test_example(driver, request):
     """
     First test
+    Запуска тестов:
+    python3 -m pytest -v -s browsers.py --browser=firefox
+    python3 -m pytest -v -s browsers.py --browser=chrome
+
     """
     driver.get(request.config.getoption('--url'))
     assert driver.title == 'Your Store'
