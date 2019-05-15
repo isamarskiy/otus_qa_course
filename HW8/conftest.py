ec77fed31d101e259db4b46140124d04cd206836
ivan <samarskyii@gmail.com>
2019-05-15 20:42:15 +0300

    HW_9_wait

--- HW8/conftest.py
+++ HW8/conftest.py
@@ -8,6 +8,7 @@ from selenium.webdriver import ChromeOptions
 def pytest_addoption(parser):
     # parser.addoption("--url", action="store", default="http://localhost/opencart")
     parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--wait", default=20000)
 
 
 @pytest.fixture(scope="session")
@@ -15,18 +16,22 @@ def driver(request):
     browser = request.config.getoption('--browser')
     if browser == 'chrome':
         print('\n Chrome browser')
+        capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+        capabilities['timeouts'] = {'implicit': int((request.config.getoption('--wait'))), 'pageLoad': 5000}
         options = ChromeOptions()
         options.add_argument('--start-maximized')
         options.add_argument('--headless')
-        wd = webdriver.Chrome(options=options)
+        wd = webdriver.Chrome(options=options, desired_capabilities=capabilities)
         request.addfinalizer(wd.quit)
         return wd
     elif browser == 'firefox':
         print('\n FF browser')
+        capabilities = webdriver.DesiredCapabilities.FIREFOX.copy()
+        capabilities['timeouts'] = {'implicit': int((request.config.getoption('--wait'))), 'pageLoad': 5000}
         options = FirefoxOptions()
         options.add_argument('--start-maximized')
         options.add_argument('--headless')
-        wd = webdriver.Firefox(options=options)
+        wd = webdriver.Firefox(options=options, desired_capabilities=capabilities)
         wd.maximize_window()
         request.addfinalizer(wd.quit)
         return wd
@@ -35,3 +40,5 @@ def driver(request):
         sys.exit(1)
     #yield wd
     wd.quit()
+
+
--- HW8/test_add_products.py
+++ HW8/test_add_products.py
@@ -1,26 +1,37 @@
 """
 Проверка добавления продукта
+
 """
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
 
 
 def test_add_product(driver):
     driver.get('http://localhost/admin')
     # http://localhost/admin http://localhost/opencart/admin
-    driver.find_element_by_id('input-username').send_keys('admin')
-    driver.find_element_by_id('input-password').send_keys('admin')
-    driver.find_element_by_css_selector('.btn.btn-primary').click()
-    driver.find_element_by_xpath("//a[contains(text(),'Catalog')]").click()
-    driver.find_element_by_xpath("//a[contains(text(),'Products')]").click()
-    driver.find_element_by_xpath("//a[@data-original-title='Add New']").click()
-    driver.find_element_by_name('product_description[1][name]').send_keys('aaTest product')
-    driver.find_element_by_name('product_description[1][meta_title]').send_keys('Test tag1')
-    driver.find_element_by_partial_link_text('Data').click()
-    driver.find_element_by_name('model').send_keys('Test model')
-    driver.find_element_by_xpath("//button[@data-original-title='Save']").click()
-    notification = driver.find_element_by_xpath("//div[contains(text(), 'Success: You have modified products')]")
-    new_product = driver.find_element_by_xpath("//td[contains(text(),'aaTest product')]").is_enabled()
-    assert new_product
-    assert notification
+    try:
+        driver.find_element_by_id('input-username').send_keys('admin')
+        driver.find_element_by_id('input-password').send_keys('admin')
+        driver.find_element_by_css_selector('.btn.btn-primary').click()
+        WebDriverWait(driver=driver, timeout=10).until(ec.title_contains('Dashboard'))
+        driver.find_element_by_xpath("//a[contains(text(),'Catalog')]").click()
+        driver.find_element_by_xpath("//a[contains(text(),'Products')]").click()
+        driver.find_element_by_xpath("//a[@data-original-title='Add New']").click()
+        WebDriverWait(driver=driver, timeout=10).until(ec.title_contains('Products'))
+        driver.find_element_by_name('product_description[1][name]').send_keys('aaTest product')
+        driver.find_element_by_name('product_description[1][meta_title]').send_keys('Test tag1')
+        driver.find_element_by_partial_link_text('Data').click()
+        driver.find_element_by_name('model').send_keys('Test model')
+        driver.find_element_by_xpath("//button[@data-original-title='Save']").click()
+        notification = driver.find_element_by_xpath("//div[contains(text(), 'Success: You have modified products')]")
+        new_product = driver.find_element_by_xpath("//td[contains(text(),'aaTest product')]").is_enabled()
+        assert new_product
+        assert notification
+    except (NoSuchElementException, ElementNotVisibleException):
+        return
+        print("Элемент отсутствует или не найден")
+
 
 
 
