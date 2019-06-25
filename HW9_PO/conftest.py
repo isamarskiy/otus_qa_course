9bb005dc274102755cd39e7fd4aac53a93d94f69
ivan <samarskyii@gmail.com>
2019-06-25 20:41:29 +0300

    HW_11_3_download_file

--- HW9_PO/conftest.py
+++ HW9_PO/conftest.py
@@ -1,8 +1,8 @@
+import os
 import sys
 import pytest
-import logging
 import urllib.parse
-from logging import Logger
+import sqlite3
 from selenium import webdriver
 from browsermobproxy import Server
 from selenium.webdriver import FirefoxOptions
@@ -10,43 +10,28 @@ from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
 
-def my_logger(log_name):
-    logger: Logger = logging.getLogger("Logs")
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh = logging.FileHandler('logs/' + log_name + '.log', mode='w')
-    # fh = logging.FileHandler("logs/new.log", mode='w')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-
-    return logger
-
-# logging.basicConfig(filename="logs/sample.log", filemode='w',  level=logging.INFO,
-#                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def my_logger(log):
+    conn = sqlite3.connect('/Users/samarsky/SQLite/db_logs.db')
+    cursor = conn.cursor()
+    sql = 'INSERT INTO Logs VALUES (' + '\'' + str(log) + '\');'
+    cursor.execute(sql)
+    conn.commit()
 
 
 class MyListener(AbstractEventListener):
 
-    def __init__(self, logger: Logger):
-        self.logger: Logger = logger
-
     def before_find(self, by, value, driver):
-        # logging.info('Поиск элемента ' + by + ' ' + value)
-        self.logger.info('Поиск элемента ' + by + ' ' + value)
+        log = 'Поиск элемента ' + by + ' ' + value
+        my_logger(log)
 
     def after_find(self, by, value, driver):
-        # logging.info('Элемент найден ' + by + ' ' + value)
-        self.logger.info('Элемент найден ' + by + ' ' + value)
-
-    def on_exception(self, exception, driver):
-        driver.save_screenshot('screenshots/new_ex.png')
-        # logging.error('Error')
-        self.logger.exception(exception)
+        log = 'Элемент найден ' + by + ' ' + value
+        my_logger(log)
 
 
 def pytest_addoption(parser):
-    # parser.addoption("--url", action="store", default="http://localhost/opencart")
-    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--url", action="store", default="http://localhost/opencart")
+    parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--wait", default=20000)
 
 
@@ -63,14 +48,15 @@ def driver(request):
     if browser == 'chrome':
         print('\n Chrome browser')
         capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-        capabilities['timeouts'] = {'implicit': int((request.config.getoption('--wait'))), 'pageLoad': 5000}
+        # capabilities['timeouts'] = {'implicit': int((request.config.getoption('--wait'))), 'pageLoad': 5000}
         capabilities['acceptSslCerts'] = True
         capabilities['acceptInsecureCerts'] = True
         options = ChromeOptions()
         options.add_argument('--start-maximized')
         options.add_argument('--headless')
         options.add_argument('--proxy-server=%s' % url)
-        wd = webdriver.Chrome(options=options, desired_capabilities=capabilities)
+        wd = EventFiringWebDriver(webdriver.Chrome(options=options, desired_capabilities=capabilities), MyListener())
+        yield wd
     elif browser == 'firefox':
         print('\n FF browser')
         capabilities = webdriver.DesiredCapabilities.FIREFOX.copy()
@@ -84,16 +70,6 @@ def driver(request):
         print('Unsupported browser')
         sys.exit(1)
 
-    print(proxy.har)
-    logger = my_logger('Logs')
-    logged_driver = EventFiringWebDriver(wd, MyListener(logger))
-    yield logged_driver
-
-    if browser == 'chrome':
-        browser_logger = my_logger('BrowsersLogs')
-        for l in logged_driver.get_log("browser"):
-            browser_logger.info(l)
-    logged_driver.quit()
 
 
 
