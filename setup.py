6cc36a54d664bea1eb68b8afdc10d93f5f3e0e99
Ivan <i.samarskiy@mangotele.com>
2019-07-23 18:52:33 +0300

    hw 31

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="first_project",
+    version="0.1",
+    url="https://github.com/isamarskiy/otus_qa_course",
+    author="Ivan",
+    author_email="samarskyii@gmail.com",
+    description="HW otus-qa-course",
+    packages=find_packages(exclude=['otus-qa-course']),
+    install_requires=['pytest>=3.7.1']
+)
